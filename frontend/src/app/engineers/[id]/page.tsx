cffc1701b15a73bab2916f22474ae98722ae337d
Saimon <mehedy303@gmail.com>
2026-04-15 23:41:05 +0600

    hehe

--- frontend/src/app/engineers/[id]/page.tsx
+++ frontend/src/app/engineers/[id]/page.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+export const runtime = 'edge';
+
 import React, { useEffect, useState } from 'react';
 import Navbar from '@/components/Navbar';
 import api, { getFileUrl } from '@/lib/api';
